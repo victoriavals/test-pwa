4c4943b2f26998ed08f56e0427ff94f074e4e88d
naufalfirdaus <naufal.firdaus@tsmid.com>
2025-05-22 10:13:19 +0700

    Push tesh ptw

---
+++ script.js
@@ -0,0 +1,6 @@
+// Daftarkan service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(() => console.log('Service Worker Registered'))
+    .catch(err => console.error('Service Worker Failed:', err));
+}
